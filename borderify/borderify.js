800dabbf267609f188392f823063a6a5dffd9bd1
gapsong <khiemtonthat@yahoo.de>
2020-11-16 00:02:39 +0100

    show correct viewport when marked

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -1,47 +1,63 @@
-document.body.style.border = '5px solid green'
-
 const createCell = (rect) => {
-    const cell = document.createElement('div')
-    cell.id = `my-element-handler`
-    cell.style.position = 'absolute'
-    cell.style.border = 'solid'
-    cell.style.width = rect.pageX - rect.x + 'px'
-    cell.style.height = rect.pageY - rect.y + 'px'
-    cell.style.left = rect.x + 'px'
-    cell.style.top = rect.y + 'px'
-    return cell
-}
+    const cell = document.createElement('div');
+    cell.id = `my-element-handler`;
+    cell.style.position = 'absolute';
+    cell.style.border = 'solid';
+    cell.style.width = rect.pageX - rect.x + 'px';
+    cell.style.height = rect.pageY - rect.y + 'px';
+    cell.style.left = rect.x + 'px';
+    cell.style.top = rect.y + 'px';
+    return cell;
+};
 
 const updateCell = (rect) => {
-    console.log(rect)
-    const cell = document.getElementById('my-element-handler')
-    cell.style.width = rect.pageX - rect.x + 'px'
-    cell.style.height = rect.pageY - rect.y + 'px'
-    cell.style.top = rect.y + 'px'
-    cell.style.left = rect.x + 'px'
-    return cell
-}
+    console.log(rect);
+    const cell = document.getElementById('my-element-handler');
+    cell.style.width = rect.pageX - rect.x + 'px';
+    cell.style.height = rect.pageY - rect.y + 'px';
+    cell.style.top = rect.y + 'px';
+    cell.style.left = rect.x + 'px';
+    return cell;
+};
 
 var rect = {
     x: 0,
     y: 0,
     pageX: 100,
     pageY: 100,
-}
+};
 
-const temp = document.getElementById('my-element-handler')
-if (temp == null) document.body.appendChild(createCell(rect))
+const temp = document.getElementById('my-element-handler');
+if (temp == null) document.body.appendChild(createCell(rect));
 
 document.body.addEventListener('mousedown', (e) => {
-    rect = { ...rect, x: e.pageX, y: e.pageY }
-    updateCell({ x: 0, y: 0, pageX: 0, pageY: 0 })
-})
+    rect = { ...rect, x: e.pageX, y: e.pageY };
+    updateCell({ x: 0, y: 0, pageX: 0, pageY: 0 });
+});
 
 document.body.addEventListener('mouseup', (e) => {
-    rect = { ...rect, pageX: e.pageX, pageY: e.pageY }
-    updateCell(rect)
-    
-    fetch('http://localhost:3000/test')
-        .then((response) => console.log(response.text()))
-        .catch()
-})
+    rect = { ...rect, pageX: e.pageX, pageY: e.pageY };
+    updateCell(rect);
+    const body = {
+        url: window.location.href,
+        rect,
+        browserSettings: {
+            innerWidth: document.body.clientWidth,
+            innerHeight: document.body.clientHeight,
+            scrollTop: document.documentElement.scrollTop,
+            scrollLeft: document.documentElement.scrollLeft,
+        },
+    };
+
+    console.log(body);
+
+    fetch('http://localhost:3000/test', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+    })
+        .then((response) => {})
+        .catch();
+});
--- main.js
+++ main.js
@@ -1,37 +1,100 @@
-const { BrowserView, BrowserWindow, app } = require('electron')
-function twoViews() {
-    const win = new BrowserWindow({ width: 800, height: 600 })
+const { BrowserView, BrowserWindow, app } = require('electron');
 
-    const view = new BrowserView()
-    win.addBrowserView(view)
-    view.setBounds({ x: 0, y: 0, width: 400, height: 300 })
-    view.webContents.loadURL('https://soundcloud.com')
+try {
+    require('electron-reloader')(module);
+} catch (_) {}
 
-    view.webContents.on('did-finish-load', async () => {
-        await view.webContents.insertCSS('html, body {width: 4000px; }')
-
-        view.webContents.executeJavaScript('window.scroll(5000, 200) ', true)
-    })
-
-    const secondView = new BrowserView()
-    win.addBrowserView(secondView)
-    secondView.setBounds({ x: 400, y: 0, width: 400, height: 300 })
-    secondView.webContents.loadURL('https://www.github.com')
-
-    const contents = secondView.webContents
-    contents.on('did-finish-load', async () => {
-        await contents.insertCSS('html, body {width: 4000px; }')
-
-        contents.executeJavaScript('window.scroll(500, 200) ', true)
-        contents.executeJavaScript('document.body.style.overflow = "hidden";', true)
-        contents.executeJavaScript('document.querySelector("html").scrollTop = window.scrollY;', true)
-    })
-
-    app.on('window-all-closed', () => {
-        win.removeBrowserView(secondView)
-        win.removeBrowserView(view)
-        app.quit()
-    })
+const viewPortConfig = {
+  url: 'https://de.wikipedia.org/wiki/Tiger',
+  rect: { x: 1169, y: 1, pageX: 1808, pageY: 155 },
+  browserSettings: { innerWidth: 1833, innerHeight: 971, scrollTop: 0, scrollLeft: 0 }
 }
 
-app.whenReady().then(twoViews)
+const rect = viewPortConfig.rect;
+const width = rect.pageX - rect.x;
+const height = rect.pageY - rect.y;
+
+const innerWidth = viewPortConfig.browserSettings.innerWidth;
+const innerHeight = viewPortConfig.browserSettings.innerHeight;
+
+const createView = () => {
+    const win = new BrowserWindow({ x: 5000, y: 5000, width: 1000, height: 500 });
+
+    const view = new BrowserView();
+    // win.addBrowserView(view)
+
+    // view.setBounds({ x: 0, y: 0, width: width, height: height })
+    // view.webContents.loadURL(viewPortConfig.url)
+
+    // view.webContents.on('did-finish-load', async () => {
+    //     await view.webContents.insertCSS(`html, body {position: relative; width: ${innerWidth}px; height:${innerHeight}px }`)
+
+    //     view.webContents.executeJavaScript(`window.scrollTo(${rect.x}, ${rect.y}) `, true)
+    // })
+
+    const secondView = new BrowserView({
+        webPreferences: {
+            nodeIntegration: true,
+            // https://www.electronjs.org/docs/api/webview-tag
+            webviewTag: true, // Security warning since Electron 10
+            zoomFactor: 1.0,
+            enableRemoteModule: true,
+        },
+    });
+    win.addBrowserView(secondView);
+    secondView.setBounds({ x: 0, y: 0, width: width, height: height });
+    secondView.webContents.loadURL('file://' + __dirname + '/index.html');
+
+    const contents = secondView.webContents;
+    contents.on('did-finish-load', async () => {
+        contents.executeJavaScript('window.scroll(500, 200) ', true);
+        contents.executeJavaScript(`window.scrollTo(${rect.x}, ${rect.y}) `, true)
+
+        contents.executeJavaScript('document.body.style.overflow = "hidden";', true);
+        contents.executeJavaScript('document.querySelector("html").scrollTop = window.scrollY;', true);
+    });
+
+    app.on('window-all-closed', () => {
+        // win.removeBrowserView(secondView)
+        win.removeBrowserView(view);
+        app.quit();
+    });
+};
+
+app.whenReady().then(createView);
+
+// let mainWindow;
+// let initPath;
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// app.allowRendererProcessReuse = true;
+// app.on("ready", () => {
+
+//   // https://www.electronjs.org/docs/api/browser-window#class-browserwindow
+//   mainWindow = new BrowserWindow({
+//     width: 1024,
+//     height: 768,
+//     //titleBarStyle: 'hidden',
+//     //frame: false,
+//     backgroundColor: "#fff",
+//     webPreferences: {
+//       nodeIntegration: true,
+//       // https://www.electronjs.org/docs/api/webview-tag
+//       webviewTag: true, // Security warning since Electron 10
+//       zoomFactor: 1.0,
+//       enableRemoteModule: true,
+//     },
+//   });
+
+//   mainWindow.loadURL("file://" + __dirname + "/index.html");
+// });
+
+// // Quit when all windows are closed.
+// app.on("window-all-closed", () => {
+//   data = {
+//     bounds: mainWindow.getBounds(),
+//   };
+//   fs.writeFileSync(initPath, JSON.stringify(data));
+//   app.quit();
+// });
