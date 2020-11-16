2697535ae2013edbb23284895a87253f49d96c45
gapsong <khiemtonthat@yahoo.de>
2020-11-17 00:01:31 +0100

    added snipping tool logic2

--- borderify/borderify.js
+++ borderify/borderify.js
@@ -1,23 +1,37 @@
-const createCell = (rect) => {
-    const cell = document.createElement('div');
-    cell.id = `my-element-handler`;
-    cell.style.position = 'absolute';
-    cell.style.border = 'solid';
-    cell.style.width = rect.pageX - rect.x + 'px';
-    cell.style.height = rect.pageY - rect.y + 'px';
-    cell.style.left = rect.x + 'px';
-    cell.style.top = rect.y + 'px';
-    return cell;
+var isHover = false;
+
+const createviewport = (rect) => {
+    const viewport = document.createElement('div');
+    viewport.id = `custom-viewport`;
+    viewport.style.position = 'absolute';
+    viewport.style.border = 'solid';
+    viewport.style.width = rect.pageX - rect.x + 'px';
+    viewport.style.height = rect.pageY - rect.y + 'px';
+    viewport.style.left = rect.x + 'px';
+    viewport.style.top = rect.y + 'px';
+    viewport.style.zIndex = '500';
+
+    viewport.addEventListener('mouseover', mouseOver);
+    viewport.addEventListener('mouseout', mouseOut);
+
+    function mouseOver() {
+        isHover = true;
+    }
+
+    function mouseOut() {
+        isHover = false;
+    }
+
+    return viewport;
 };
 
-const updateCell = (rect) => {
-    console.log(rect);
-    const cell = document.getElementById('my-element-handler');
-    cell.style.width = rect.pageX - rect.x + 'px';
-    cell.style.height = rect.pageY - rect.y + 'px';
-    cell.style.top = rect.y + 'px';
-    cell.style.left = rect.x + 'px';
-    return cell;
+const updateviewport = (rect) => {
+    const viewport = document.getElementById('custom-viewport');
+    viewport.style.width = rect.pageX - rect.x + 'px';
+    viewport.style.height = rect.pageY - rect.y + 'px';
+    viewport.style.top = rect.y + 'px';
+    viewport.style.left = rect.x + 'px';
+    return viewport;
 };
 
 var rect = {
@@ -27,37 +41,110 @@ var rect = {
     pageY: 100,
 };
 
-const temp = document.getElementById('my-element-handler');
-if (temp == null) document.body.appendChild(createCell(rect));
+const createMenu = () => {
+    const button = document.createElement('input');
+    button.id = 'start-button';
+    button.type = 'button';
+    button.value = 'text';
+    button.style.position = 'absolute';
+    button.style.left = '0px';
+    button.style.top = '0px';
+    button.style.zIndex = '500';
+    button.addEventListener('click', function () {
+        document.documentElement.style.cursor = 'crosshair';
+    });
+    return button;
+};
+
+const init = () => {
+    document.body.appendChild(createMenu());
+
+    const temp = document.getElementById('my-element-handler');
+    if (temp == null) {
+        document.body.appendChild(createviewport(rect));
+    }
+};
 
 document.body.addEventListener('mousedown', (e) => {
-    rect = { ...rect, x: e.pageX, y: e.pageY };
-    updateCell({ x: 0, y: 0, pageX: 0, pageY: 0 });
+    if (!isHover) {
+        rect = { ...rect, x: e.pageX, y: e.pageY };
+        updateviewport({ x: 0, y: 0, pageX: 0, pageY: 0 });
+    }
 });
 
 document.body.addEventListener('mouseup', (e) => {
-    rect = { ...rect, pageX: e.pageX, pageY: e.pageY };
-    updateCell(rect);
-    const body = {
-        url: window.location.href,
-        rect,
-        browserSettings: {
-            innerWidth: window.innerWidth,
-            innerHeight: window.innerHeight,
-            scrollTop: document.documentElement.scrollTop,
-            scrollLeft: document.documentElement.scrollLeft,
-        },
-    };
+    if (!isHover) {
+        rect = { ...rect, pageX: e.pageX, pageY: e.pageY };
+        updateviewport(rect);
+        const body = {
+            url: window.location.href,
+            rect,
+            browserSettings: {
+                innerWidth: window.innerWidth,
+                innerHeight: window.innerHeight,
+                scrollTop: document.documentElement.scrollTop,
+                scrollLeft: document.documentElement.scrollLeft,
+            },
+        };
 
-    console.log(body);
+        postViewport(body);
+    }
+});
 
+const postViewport = (body) =>
     fetch('http://localhost:3000/test', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(body),
-    })
-        .then((response) => {})
-        .catch();
-});
+    });
+
+init();
+
+// Make the DIV element draggable:
+dragElement(document.getElementById('custom-viewport'));
+
+function dragElement(elmnt) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementById(elmnt.id + 'header')) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + 'header').onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = elmnt.offsetTop - pos2 + 'px';
+        elmnt.style.left = elmnt.offsetLeft - pos1 + 'px';
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
